b1920ceaf135c0ca98b9cf6a6a43a532dff7eea0
Lybbe <k.lybech91@gmail.com>
2024-10-19 10:37:27 +0200

    added content

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/image.png")
+with col2:
+    st.title("Kenneth Lybech")
+    content = """
+    Hello. I am Kenneth! I am a Python programmer. I am selfeducated through courses on udemy and selfstudy.
+    I am new to Python coding.
+    """
+    st.write(content)
